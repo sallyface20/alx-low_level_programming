92bb25ea8b6cf28193f6e3939f40ddd9737730ea
amine sbay <aminesbayicloud@gmail.com>
2023-08-11 12:43:41 -0700

    fgf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/**
- * main - A program that prints a line with puts fonction
- * Return: 0 (Success)
- */
-int main(void)
+
+int	main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle"\n);
 return (0);
 }

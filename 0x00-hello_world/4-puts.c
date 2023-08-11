bbc36d84e66676102b26c40293ff4a2d210b4f09
amine sbay <aminesbayicloud@gmail.com>
2023-08-10 18:35:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - A program that prints a line with puts fonction
+ * Return: 0 (success)
+ *
  */
 int maid(void)
 {

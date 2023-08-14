15d58ad368e0093d1839c4d89bd9c92fe0a03359
amine sbay <aminesbayicloud@gmail.com>
2023-08-14 13:06:44 -0700

    wrf

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase and uppercase
+* Return: Always 0.
+*/
+
+int main(void)
+{
+char letter;
+char Letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar (letter);
+for (Letter = 'A'; Letter <= 'Z'; Letter++)
+putchar (Letter);
+putchar ('\n');
+
+return (0);
+}
